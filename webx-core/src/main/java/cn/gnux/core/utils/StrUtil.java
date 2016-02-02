c197fe7f2b19cea0b194c9d1d19ccd6b5e9f17b1
d0f <xiaofeng_zhao@qq.com>
2016-02-02 16:20:11 +0800

    全角,半角切换

--- webx-core/src/main/java/cn/gnux/core/utils/StrUtil.java
+++ webx-core/src/main/java/cn/gnux/core/utils/StrUtil.java
@@ -4,17 +4,19 @@ package cn.gnux.core.utils;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 字符串工具类
  * 
- * @author xiaoleilu
  *
  */
 public class StrUtil {
@@ -250,7 +252,31 @@ public class StrUtil {
 	public static String upperFirst(String str) {
 		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
 	}
+	
+	/**
+     * 将字符串首字母大写后返回
+     * 
+     * @param str 原字符串
+     * @return 首字母大写后的字符串
+     * 
+     * <pre>
+     *      capitalizeFirstLetter(null)     =   null;
+     *      capitalizeFirstLetter("")       =   "";
+     *      capitalizeFirstLetter("2ab")    =   "2ab"
+     *      capitalizeFirstLetter("a")      =   "A"
+     *      capitalizeFirstLetter("ab")     =   "Ab"
+     *      capitalizeFirstLetter("Abc")    =   "Abc"
+     * </pre>
+     */
+    public static String capitalizeFirstLetter(String str) {
+        if (isEmpty(str)) {
+            return str;
+        }
 
+        char c = str.charAt(0);
+        return (!Character.isLetter(c) || Character.isUpperCase(c)) ? str : new StringBuilder(str.length()).append(Character.toUpperCase(c)).append(str.substring(1)).toString();
+    }
+	
 	/**
 	 * 小写首字母<br>
 	 * 例如：str = Name, return name
@@ -938,4 +964,173 @@ public class StrUtil {
 	public static StringWriter getWriter() {
 		return new StringWriter();
 	}
+	
+    /**
+     * 如果不是普通字符，则按照utf8进行编码
+     * 
+     * <pre>
+     * utf8Encode(null)        =   null
+     * utf8Encode("")          =   "";
+     * utf8Encode("aa")        =   "aa";
+     * utf8Encode("啊啊啊啊")   = "%E5%95%8A%E5%95%8A%E5%95%8A%E5%95%8A";
+     * </pre>
+     * 
+     * @param str 原字符
+     * @return 编码后字符，若编码异常抛出异常
+     */
+	public static String utf8Encode(String str) {
+		if(!isEmpty(str) && str.getBytes().length != str.length()) {
+			try {
+				return URLEncoder.encode(str,"UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				 throw new RuntimeException("UnsupportedEncodingException occurred. ", e);
+			}
+		}
+		return str;
+	}
+	
+	/**
+     * 如果不是普通字符，则按照utf8进行编码，编码异常则返回defultReturn
+     * 
+     * @param str 源字符串
+     * @param defultReturn 出现异常默认返回
+     * @return
+     */
+	public static String utf8Encode(String str, String defaultReturn) {
+		if (!isEmpty(str) && str.getBytes().length != str.length()) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return defaultReturn;
+            }
+        }
+        return str;
+	}
+	
+	/**
+     * 得到href链接的innerHtml
+     * 
+     * @param href href内容
+     * @return href的innerHtml
+     *         <ul>
+     *         <li>空字符串返回""</li>
+     *         <li>若字符串不为空，且不符合链接正则的返回原内容</li>
+     *         <li>若字符串不为空，且符合链接正则的返回最后一个innerHtml</li>
+     *         </ul>
+     * @see
+     * <pre>
+     *      getHrefInnerHtml(null)                                  = ""
+     *      getHrefInnerHtml("")                                    = ""
+     *      getHrefInnerHtml("mp3")                                 = "mp3";
+     *      getHrefInnerHtml("&lt;a innerHtml&lt;/a&gt;")                    = "&lt;a innerHtml&lt;/a&gt;";
+     *      getHrefInnerHtml("&lt;a&gt;innerHtml&lt;/a&gt;")                    = "innerHtml";
+     *      getHrefInnerHtml("&lt;a&lt;a&gt;innerHtml&lt;/a&gt;")                    = "innerHtml";
+     *      getHrefInnerHtml("&lt;a href="baidu.com"&gt;innerHtml&lt;/a&gt;")               = "innerHtml";
+     *      getHrefInnerHtml("&lt;a href="baidu.com" title="baidu"&gt;innerHtml&lt;/a&gt;") = "innerHtml";
+     *      getHrefInnerHtml("   &lt;a&gt;innerHtml&lt;/a&gt;  ")                           = "innerHtml";
+     *      getHrefInnerHtml("&lt;a&gt;innerHtml&lt;/a&gt;&lt;/a&gt;")                      = "innerHtml";
+     *      getHrefInnerHtml("jack&lt;a&gt;innerHtml&lt;/a&gt;&lt;/a&gt;")                  = "innerHtml";
+     *      getHrefInnerHtml("&lt;a&gt;innerHtml1&lt;/a&gt;&lt;a&gt;innerHtml2&lt;/a&gt;")        = "innerHtml2";
+     * </pre>
+     */
+    public static String getHrefInnerHtml(String href) {
+        if (isEmpty(href)) {
+            return "";
+        }
+        String hrefReg = ".*<[\\s]*a[\\s]*.*>(.+?)<[\\s]*/a[\\s]*>.*";
+        Pattern hrefPattern = Pattern.compile(hrefReg, Pattern.CASE_INSENSITIVE);
+        Matcher hrefMatcher = hrefPattern.matcher(href);
+        if (hrefMatcher.matches()) {
+            return hrefMatcher.group(1);
+        }
+        return href;
+    }
+    
+    /**
+     * html的转义字符转换成正常的字符串
+     * 
+     * <pre>
+     * htmlEscapeCharsToString(null) = null;
+     * htmlEscapeCharsToString("") = "";
+     * htmlEscapeCharsToString("mp3") = "mp3";
+     * htmlEscapeCharsToString("mp3&lt;") = "mp3<";
+     * htmlEscapeCharsToString("mp3&gt;") = "mp3\>";
+     * htmlEscapeCharsToString("mp3&amp;mp4") = "mp3&mp4";
+     * htmlEscapeCharsToString("mp3&quot;mp4") = "mp3\"mp4";
+     * htmlEscapeCharsToString("mp3&lt;&gt;&amp;&quot;mp4") = "mp3\<\>&\"mp4";
+     * </pre>
+     * 
+     * @param source
+     * @return
+     */
+    public static String htmlEscapeCharsToString(String source) {
+        if (StrUtil.isEmpty(source)) {
+            return source;
+        } else {
+            return source.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&amp;", "&").replaceAll("&quot;",
+                                                                                                              "\"");
+        }
+    }
+    
+    /**
+     * 半角字符转换为全角字符
+     * 
+     * <pre>
+     * fullWidthToHalfWidth(null) = null;
+     * fullWidthToHalfWidth("") = "";
+     * fullWidthToHalfWidth(new String(new char[] {12288})) = " ";
+     * fullWidthToHalfWidth("！＂＃＄％＆) = "!\"#$%&";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String fullWidthToHalfWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == 12288) {
+                source[i] = ' ';
+            } else if (source[i] >= 65281 && source[i] <= 65374) {
+                source[i] = (char)(source[i] - 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+    /**
+     * 全角字符转换为半角字符
+     * 
+     * <pre>
+     * halfWidthToFullWidth(null) = null;
+     * halfWidthToFullWidth("") = "";
+     * halfWidthToFullWidth(" ") = new String(new char[] {12288});
+     * halfWidthToFullWidth("!\"#$%&) = "！＂＃＄％＆";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String halfWidthToFullWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == ' ') {
+                source[i] = (char)12288;
+                // } else if (source[i] == '.') {
+                // source[i] = (char)12290;
+            } else if (source[i] >= 33 && source[i] <= 126) {
+                source[i] = (char)(source[i] + 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
 }
