a135c738b177daba0f9448911518727f3ea92c03
d0f <xiaofeng_zhao@qq.com>
2016-02-03 09:51:38 +0800

    RandomUtil 字母工具

--- webx-core/src/main/java/cn/gnux/core/utils/RandomUtil.java
+++ webx-core/src/main/java/cn/gnux/core/utils/RandomUtil.java
@@ -14,12 +14,6 @@ import java.util.UUID;
 public class RandomUtil {
 	private static Random random = new Random();
 	
-	
-//	private static final String BASE_NUMBER = "0123456789";
-//	/** 用于随机选的字符 */
-//	private static final String BASE_CHAR = "abcdefghijklmnopqrstuvwxyz";
-//	
-//	private static final String BASE_CHAR_NUMBER = BASE_CHAR + BASE_NUMBER;
 	/** 用于随机选的字符和数字 */
 	public static final String NUMBERS_AND_LETTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	/** 用于随机选的数字 */
@@ -133,6 +127,102 @@ public class RandomUtil {
 		return sb.toString();
 	}
 	
+	 /**
+     * 得到固定长度的随机字符串，字符串由数字和大小写字母混合组成
+     * 
+     * @param length 长度
+     * @return
+     * @see 见{@link StringUtils#getRandom(String source, int length)}
+     */
+    public static String getRandomNumbersAndLetters(int length) {
+        return getRandom(NUMBERS_AND_LETTERS, length);
+    }
+    
+    /**
+     * 得到固定长度的随机字符串，字符串由数字混合组成
+     * 
+     * @param length 长度
+     * @return
+     * @see 见{@link StringUtils#getRandom(String source, int length)}
+     */
+    public static String getRandomNumbers(int length) {
+        return getRandom(NUMBERS, length);
+    }
+    
+    /**
+     * 得到固定长度的随机字符串，字符串由大小写字母混合组成
+     * 
+     * @param length 长度
+     * @return
+     * @see 见{@link StringUtils#getRandom(String source, int length)}
+     */
+    public static String getRandomLetters(int length) {
+        return getRandom(LETTERS, length);
+    }
+    
+    
+
+    /**
+     * 得到固定长度的随机字符串，字符串由大写字母混合组成
+     * 
+     * @param length 长度
+     * @return
+     * @see 见{@link StringUtils#getRandom(String source, int length)}
+     */
+    public static String getRandomCapitalLetters(int length) {
+        return getRandom(CAPITAL_LETTERS, length);
+    }
+
+    /**
+     * 得到固定长度的随机字符串，字符串由小写字母混合组成
+     * 
+     * @param length 长度
+     * @return
+     * @see 见{@link StringUtils#getRandom(String source, int length)}
+     */
+    public static String getRandomLowerCaseLetters(int length) {
+        return getRandom(LOWER_CASE_LETTERS, length);
+    }
+    
+    /**
+     * 得到固定长度的随机字符串，字符串由source中字符混合组成
+     * 
+     * @param source 源字符串
+     * @param length 长度
+     * @return
+     *         <ul>
+     *         <li>若source为null或为空字符串，返回null</li>
+     *         <li>否则见{@link StringUtils#getRandom(char[] sourceChar, int length)}</li>
+     *         </ul>
+     */
+    public static String getRandom(String source, int length) {
+        return StrUtil.isEmpty(source) ? null : getRandom(source.toCharArray(), length);
+    }
+
+    /**
+     * 得到固定长度的随机字符串，字符串由sourceChar中字符混合组成
+     * 
+     * @param sourceChar 源字符数组
+     * @param length 长度
+     * @return
+     *         <ul>
+     *         <li>若sourceChar为null或长度为0，返回null</li>
+     *         <li>若length小于0，返回null</li>
+     *         </ul>
+     */
+    public static String getRandom(char[] sourceChar, int length) {
+        if (sourceChar == null || sourceChar.length == 0 || length < 0) {
+            return null;
+        }
+
+        StringBuilder str = new StringBuilder(length);
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            str.append(sourceChar[random.nextInt(sourceChar.length)]);
+        }
+        return str.toString();
+    }
+    
 	/**
 	 * @return 随机UUID
 	 */
